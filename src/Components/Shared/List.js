5e9c2f2246185a4ccbbbbdda8b8d46e281d42621
Gabriel Mičko <micsko@gmail.com>
2018-05-12 08:23:26 +0200

    Update with differente modes

--- src/Components/Shared/List.js
+++ src/Components/Shared/List.js
@@ -1,20 +1,35 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { title } from '../../Config/config.json';
+import { mode } from '../Config/config.json';
 
 class List extends React.Component {
   render() {
-    return (
+    if (mode === 'firefox') {
       <div className="shared list">
-        <h3>You have seen</h3>
+        <h3>Firefox Quantum</h3>
         <ul>
-          <li>The new Firefox Quantum</li>
-          <li>WebVR JavaScript API</li>
-          <li>A-Frame, web framework for building virtual reality experiences</li>
-          <li>HTC Vive is a virtual reality headset</li>
+          <li>lightning fast</li>
+          <li>secure</li>
+          <li>great design</li>
+          <li>smart features</li>
         </ul>
-      </div>
-    );
+      </div>;
+    } else {
+      return (
+        <div className="shared list">
+          <h3>You have seen</h3>
+          <ul>
+            <li>The new Firefox Quantum</li>
+            <li>WebVR JavaScript API</li>
+            <li>
+              A-Frame, web framework for building virtual reality experiences
+            </li>
+            <li>HTC Vive is a virtual reality headset</li>
+          </ul>
+        </div>
+      );
+    }
   }
 }
 
